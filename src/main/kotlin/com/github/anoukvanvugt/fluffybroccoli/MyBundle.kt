3d251602d02d5908926260568701e72f8eb350a5
GitHub Action <action@github.com>
2021-06-21 12:14:53 +0000

    Template cleanup

--- src/main/kotlin/com/github/anoukvanvugt/fluffybroccoli/MyBundle.kt
+++ src/main/kotlin/com/github/anoukvanvugt/fluffybroccoli/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.anoukvanvugt.fluffybroccoli
 
 import com.intellij.AbstractBundle
 import org.jetbrains.annotations.NonNls
---
+++ src/main/kotlin/com/github/anoukvanvugt/fluffybroccoli/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.anoukvanvugt.fluffybroccoli.services
+
+import com.github.anoukvanvugt.fluffybroccoli.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
